0ecf6997a81d8bf16c807e27990f5eed41c484b3
goshi0504 <temkazero8@gmail.com>
2026-04-25 15:45:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' || req.method === 'HEAD') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    if (req.method === 'GET') res.write('Bot is online!');
+    res.end();
+  } else {
+    res.writeHead(501);
+    res.end();
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Keep-alive server running on port ${PORT}`);
+});
+
+// --- ДООШ ҮНДСЭН КОД ҮРГЭЛЖИЛНЭ ---
 import { Client, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 import { handleInteraction } from './discord/interactionHandler.js';
